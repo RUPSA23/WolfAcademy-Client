fe08c0f6b8a2765fdb47736d25489d3bd13ebc57
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-12-27 13:01:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './Components/Contact/Contact';
+import Home from './Components/Home/Home';
+import Footer from './Components/Shared/Footer';
+import Navbar from './Components/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Home/Banner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full ">
+            <div id="item1" className="carousel-item w-full">
+    <img src="https://i.pinimg.com/736x/ac/01/44/ac014446e4c62e078975a3b603f67938.jpg" className="w-full h-96" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://cdnblog.etmoney.com/wp-content/uploads/2021/09/investing-at-market-high-1.jpg" className="w-full h-96" />
+  </div> 
+ 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://www.amakella.com/wp-content/uploads/2020/08/how-the-stock-market-works-1024x576.jpg" className="w-full h-96" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="https://cdn.dnaindia.com/sites/default/files/styles/full/public/2019/08/21/860914-stockmarket-istock-111518.jpg" className="w-full h-96" />
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
